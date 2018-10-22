196b82cdc563a1bf485d63cc0b3351ddc8db974b
Frédéric Delorme <frederic.delorme@snapgames.fr>
2018-10-22 02:02:09 +0200

    remove unecessary dependengy.

--- src/main/java/fr/snapgames/bgf/Window.java
+++ src/main/java/fr/snapgames/bgf/Window.java
@@ -7,7 +7,6 @@ import java.awt.GraphicsEnvironment;
 import java.awt.Insets;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
