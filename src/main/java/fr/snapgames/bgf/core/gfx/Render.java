c0fd8419e4e243ad782ac655daca6c67fa590416
Frédéric Delorme <frederic.delorme@snapgames.fr>
2019-01-09 22:43:29 +0100

    add a Player entity

--- src/main/java/fr/snapgames/bgf/core/gfx/Render.java
+++ src/main/java/fr/snapgames/bgf/core/gfx/Render.java
@@ -24,8 +24,6 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
